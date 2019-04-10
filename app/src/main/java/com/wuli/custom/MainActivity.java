9256537c064fbdda0b25506681ce09f75c6b1928
王康 <wangkang@xiaomalixing.com>
2019-04-10 17:29:18 +0800

    初始项目

---
+++ app/src/main/java/com/wuli/custom/MainActivity.java
@@ -0,0 +1,26 @@
+package com.wuli.custom;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext=getApplicationContext();
+        nitificationTest();
+
+    }
+    public void nitificationTest(){
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this).setSmallIcon(R.drawable.ic_launcher_background);
+        Notification notification=builder.build();
+        NotificationManager notificationManager=(NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(200,notification);
+    }
+}
---
+++ app/src/main/java/com/wuli/custom/customView/RectTestView.java
@@ -0,0 +1,91 @@
+package com.wuli.custom.customView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.wuli.custom.Tools.WLog;
+
+/**
+ * <pre>
+ *     author : wangkang
+ *     time   : 2019/03/30
+ *     desc   :
+ * </pre>
+ */
+public class RectTestView extends View {
+    public Paint mPaint;
+    public RectTestView(Context context) {
+        super(context);
+        WLog.i("RectTestView()1");
+        init();
+
+    }
+
+    public RectTestView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        WLog.i("RectTestView()2");
+        init();
+    }
+
+    public RectTestView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        WLog.i("RectTestView()3");
+        init();
+    }
+
+    public RectTestView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        WLog.i("RectTestView()4");
+        init();
+    }
+    RectF rectF;
+    public void init(){
+        WLog.i("init()");
+        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.parseColor("#000000"));
+        rectF=new RectF();
+        rectF.set(1,1,99,300);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        WLog.i("onDraw()");
+        int layerId = canvas.saveLayer(0, 0, canvas.getWidth(), canvas.getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        drawTest(canvas);
+
+        mPaint.setColor(Color.BLACK);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        canvas.drawRect(rectF,mPaint);
+        canvas.restoreToCount(layerId);
+        rectF.top=100;
+//        invalidate();
+    }
+    public void drawTest(Canvas canvas) {
+        WLog.i("drawTest()");
+        Paint mPaint=new Paint();
+        mPaint.setColor(Color.RED);
+        RectF rectF1=new RectF();
+        rectF1.set(1,1,99,300);
+
+        canvas.drawRoundRect(rectF1,49,49,mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        WLog.i("event()"+event.getAction());
+        return true;
+//        return super.onTouchEvent(event);
+    }
+}
