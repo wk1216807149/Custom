3d9d4f9d1a2308504dba42c22f437b54a3808aeb
王康 <wangkang@xiaomalixing.com>
2019-06-26 20:32:09 +0800

    2019/01/01 iioo

---
+++ Utils/src/main/java/com/wuli/utils/StringTools.java
@@ -0,0 +1,14 @@
+package com.wuli.utils;
+
+/**
+ * <pre>
+ *     author : wangkang
+ *     time   : 2019/05/23
+ *     desc   :
+ * </pre>
+ */
+public class StringTools {
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+}
