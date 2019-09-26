1aaa7f48f646c0397a4eccd07753db0ab4d547ae
wangkang <wk1216807149@163.com>
2019-09-26 23:28:00 +0800

    B

--- Utils/src/main/java/com/wuli/utils/file/FileUtils.java
+++ Utils/src/main/java/com/wuli/utils/file/FileUtils.java
@@ -15,69 +15,53 @@ import java.util.Set;
  */
 public class FileUtils {
 
-    public static int ModePrivate = Context.MODE_PRIVATE;
+    public static int ModePrivate=Context.MODE_PRIVATE;
     @Deprecated
-    public static int ModeReadable = Context.MODE_WORLD_READABLE;
+    public static int ModeReadable=Context.MODE_WORLD_READABLE;
     @Deprecated
-    public static int ModeWriteable = Context.MODE_WORLD_WRITEABLE;
-    public static int ModeProcess = Context.MODE_MULTI_PROCESS;
+    public static int ModeWriteable=Context.MODE_WORLD_WRITEABLE;
+    public static int ModeProcess=Context.MODE_MULTI_PROCESS;
 
-    private SharedPreferences sharedPreferences = null;
+    private static SharedPreferences sharedPreferences=null;
 
-    public SharedPreferences getSharedPreferences() {
-        return sharedPreferences;
-    }
-
-    public void setSharedPreferences(SharedPreferences sharedPreferences) {
-        this.sharedPreferences = sharedPreferences;
-    }
-
-    public void createSharedPreferences(Context context, String str, int mod) {
-        sharedPreferences = context.getSharedPreferences(str, mod);
+    public static void createSharedPreferences(Context context,String str,int mod){
+        sharedPreferences=context.getSharedPreferences(str,mod);
 
     }
 
-    public void saveDataSharedPreferences(String name, Object object) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        if (object instanceof String) {
-            editor.putString(name, (String) object);
-        } else if (object instanceof Integer) {
-            editor.putInt(name, (int) object);
-        } else if (object instanceof Boolean) {
-            editor.putBoolean(name, (boolean) object);
-        } else if (object instanceof Float) {
-            editor.putFloat(name, (float) object);
-        } else if (object instanceof Long) {
-            editor.putLong(name, (long) object);
-        } else if (object instanceof Set) {
-            editor.putStringSet(name, (Set<String>) object);
+    public static void saveDataSharedPreferences(String name,Object object){
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        if(object instanceof String){
+            editor.putString(name,(String) object);
+        }else if(object instanceof Integer){
+            editor.putInt(name,(int)object);
+        }else if(object instanceof Boolean){
+            editor.putBoolean(name,(boolean)object);
+        }else if(object instanceof Float){
+            editor.putFloat(name,(float)object);
+        }else if(object instanceof Long){
+            editor.putLong(name,(long)object);
+        }else if(object instanceof Set){
+            editor.putStringSet(name,(Set<String>) object);
         }
         editor.commit();
-
     }
 
-    public Object obtainDataSharedPreferences(String name, Object object) {
-        if (object instanceof String) {
-            return sharedPreferences.getString(name, (String) object);
-        } else if (object instanceof Integer) {
-            return sharedPreferences.getInt(name, (int) object);
-        } else if (object instanceof Boolean) {
-            return sharedPreferences.getBoolean(name, (boolean) object);
-        } else if (object instanceof Float) {
-            return sharedPreferences.getFloat(name, (float) object);
-        } else if (object instanceof Long) {
-            return sharedPreferences.getLong(name, (long) object);
-        } else if (object instanceof Set) {
-            return sharedPreferences.getStringSet(name, (Set<String>) object);
-        } else {
+    public static Object getDataSharedPreferences(String name,Object object){
+        if(object instanceof String){
+            return sharedPreferences.getString(name,(String)object);
+        }else if(object instanceof Integer){
+            return sharedPreferences.getInt(name,(int)object);
+        }else if(object instanceof Boolean){
+            return sharedPreferences.getBoolean(name,(boolean)object);
+        }else if(object instanceof Float){
+            return sharedPreferences.getFloat(name,(float)object);
+        }else if(object instanceof Long){
+            return sharedPreferences.getLong(name,(long)object);
+        }else if(object instanceof Set){
+            return sharedPreferences.getStringSet(name,(Set<String>) object);
+        }else{
             return sharedPreferences.getAll();
         }
-    }
-
-    @Override
-    public String toString() {
-        return "FileUtils{" +
-                "sharedPreferences=" + sharedPreferences +
-                '}';
     }
 }
